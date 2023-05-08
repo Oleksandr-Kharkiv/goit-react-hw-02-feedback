288ebdfd7eaebf35c53e7ad7432e358d410591d3
Oleksandr-Kharkiv <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-05-08 17:15:47 +0200

    fix FeedbackOptions

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -2,12 +2,19 @@ import PropTypes from 'prop-types';
 import css from './FeedbackOptions.module.css';
 
 export const FeedbackOptions = ({ onLeaveFeedback, options }) => {
-  
   return (
     <div className={css.buttons}>
-      <button type="button" onClick={onLeaveFeedback} className={css.btn} name="good">good</button>
-      <button type="button" onClick={onLeaveFeedback} className={css.btn} name="neutral">neutral</button>
-      <button type="button" onClick={onLeaveFeedback} className={css.btn} name="bad">bad</button>
+      {Object.keys(options).map(arrayEl => (
+        <button
+          type="button"
+          key={arrayEl}
+          onClick={onLeaveFeedback}
+          className={css.btn}
+          name={arrayEl}
+        >
+          {arrayEl}
+        </button>
+      ))}
     </div>
   );
 };
